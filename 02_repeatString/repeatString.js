d823ec2a5fc927c4fe4181c2e867d941eeb564f7
trakkito <158862513+trakkito@users.noreply.github.com>
2024-07-04 11:15:57 -0400

    finished 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    if (number < 0) {
+        return "ERROR"
+    };
+    let string = "";
+    for (let i = 0; i < number; i++) {
+        string += word;
+    }
+    return string
 };
+
+console.log(repeatString("", 10))
 
 // Do not edit below this line
 module.exports = repeatString;
