d1c911ffa654a5182b8f9c89d94b1566206c2a1d
trakkito <158862513+trakkito@users.noreply.github.com>
2024-07-04 11:47:59 -0400

    completed 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    return word.split("").reverse().join();
 };
+
+console.log(reverseString("hotdog"))
 
 // Do not edit below this line
 module.exports = reverseString;
