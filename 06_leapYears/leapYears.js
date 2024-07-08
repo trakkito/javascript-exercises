d36df8b9276ef4c11cd0aa241fbbe5edfe47fc82
trakkito <158862513+trakkito@users.noreply.github.com>
2024-07-08 19:51:46 -0400

    finished 06_leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+     if (isNaN(year) || year < 0) {
+        return "Not a valid year";
+    } else if (year % 400 === 0) {
+        return true;
+    } else if (year % 100 === 0) {
+        return false;
+    } else if (year % 4 === 0) {
+        return true;
+    } else {
+       return false;
+    }
 };
+
+console.log(leapYears(700))
 
 // Do not edit below this line
 module.exports = leapYears;
