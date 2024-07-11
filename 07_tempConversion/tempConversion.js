9443b0469b83f9ea2845bf0b6cefc69edcf82846
trakkito <158862513+trakkito@users.noreply.github.com>
2024-07-11 17:36:53 -0400

    finished 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  if (isNaN(temp)) return "ERROR";
+  let fahrenheit = 32;
+  let celsius = (temp-fahrenheit)*5/9;
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  if (isNaN(temp)) return "ERROR";
+  let fahrenheit = 32;
+  let celsius = (temp*9/5)+fahrenheit;
+  return parseFloat(celsius.toFixed(1));
 };
+
+console.log(convertToFahrenheit(-10))
+
+console.log(convertToCelsius(-100))
 
 // Do not edit below this line
 module.exports = {
